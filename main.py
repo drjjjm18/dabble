699af2baced137480d9f167491f40d8dcf1df268
drjjjm18 <drjjm18@gmail.com>
2021-12-12 23:23:26 +0000

    main game functionality

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.recycleview import RecycleView
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.properties import ObjectProperty, ListProperty, NumericProperty, DictProperty, StringProperty
 from random import choice, randint, uniform
 from kivy.support import install_twisted_reactor
@@ -27,7 +28,7 @@ class Client(LineReceiver):
         self.app = App.get_running_app()
 
     def connectionMade(self):
-        self.transport.write(pickle.dumps(['join', self.app.name]))
+        self.sendLine(pickle.dumps(['join', self.app.name]))
 
     def lineReceived(self, line):
         msg = pickle.loads(line)
@@ -46,12 +47,20 @@ class Client(LineReceiver):
             return
         if msg[0] == 'next':
             self.app.gm.update_cards(deck=msg[1])
+            text = f'{msg[2]} got that one!'
+            self.app.gm.display_text = text
             return
         if msg[0] == 'win':
             self.app.gm.update_cards(deck=msg[1], card=self.app.gm.ids.deck.images)
-            self.app.gm.total_cards += 1
+            text = f'you got that one!'
+            self.app.gm.display_text = text
             return
-
+        if msg[0] == 'over':
+            self.app.gm.display_text = 'GAME FINISHED'
+            self.app.gm.update_cards(deck=[0, 0, 0, 0, 0, 0, 0, 0])
+            Clock.schedule_once(lambda dt: setattr(self.app.sm, 'current', 'results'), 2)
+            self.app.results.scores, self.app.results.result_string = msg[1], msg[2]
+            return
         return
 
 
@@ -69,6 +78,26 @@ class NewClientFactory(ClientFactory):
 
     def clientConnectionFailed(self, connector, reason):
         print('Connection failed. Reason:', reason)
+
+
+class Results(Screen):
+
+    scores = DictProperty()
+    results_string = StringProperty()
+    data = DictProperty()
+    display_string = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(Results, self).__init__(**kwargs)
+        App.get_running_app().results = self
+
+    def on_enter(self, *args):
+        # for x in range(len(self.scores.keys())):
+        #     Clock.schedule_once(lambda dt: setattr(self, 'data', [{'text': f'{y}: {self.scores[y]}'} for y in list(self.scores.keys())[:x+1]]), x+1)
+        # Clock.schedule_once(lambda dt: setattr(self, 'display_string', self.results_string), len(self.scores.keys())+1)
+        for x in self.scores.keys():
+            Lab = Label(text=f'{x} scored: {self.scores[x]}')
+            self.add_widget(Lab)
 
 
 class GameScreen(Screen):
@@ -91,11 +120,7 @@ class GameScreen(Screen):
 
     def matched(self):
         app = App.get_running_app()
-        app.connection.transport.write(pickle.dumps(['match']))
-
-    # def on_touch_down(self, touch):
-    #     self.ids.card.images = choice(self.cards)
-    #     self.ids.deck.images = choice(self.cards)
+        app.connection.sendLine(pickle.dumps(['match']))
 
     def on_enter(self, *args):
         self.count_down()
@@ -114,8 +139,12 @@ class GameScreen(Screen):
         if deck is not None:
             self.ids.deck.images = deck
 
-    def win(self):
-        self.ids
+    def leave_game(self):
+        app = App.get_running_app()
+        app.connection.sendLine(pickle.dumps(['left']))
+        app.sm.current = 'lobby'
+        ims = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.ids.card.images, self.ids.deck.images = ims, ims
 
 
 class EnterName(Screen):
@@ -123,7 +152,7 @@ class EnterName(Screen):
     def enter_game(self):
         app = App.get_running_app()
         if app.name == '' or str(app.name).lower() == 'amrik':
-            app.name = 'xXxCAT4LYF'+str(randint(10, 99))+'xXx'
+            app.name = 'xXxCAT4LYF_'+str(randint(10, 99))+'xXx'
         app.connect_to_server()
 
 
@@ -139,13 +168,16 @@ class Lobby(Screen):
     def ready_press(self):
         if self.ready_text == 'Not ready':
             self.ready_text = 'READY!'
-            App.get_running_app().connection.transport.write(pickle.dumps(['ready', 'y']))
+            App.get_running_app().connection.sendLine(pickle.dumps(['ready', 'y']))
         else:
             self.ready_text = 'Not ready'
-            App.get_running_app().connection.transport.write(pickle.dumps(['ready', 'n']))
+            App.get_running_app().connection.sendLine(pickle.dumps(['ready', 'n']))
 
     def begin_press(self):
-        App.get_running_app().connection.transport.write(pickle.dumps(['begin']))
+        App.get_running_app().connection.sendLine(pickle.dumps(['begin']))
+
+    def on_enter(self, *args):
+        self.ready_text = 'Not ready'
 
 
 class RV(RecycleView):
@@ -232,6 +264,7 @@ class DabbleApp(App):
     lobby = ObjectProperty()
     name = StringProperty('')
     rv = ObjectProperty()
+    results = ObjectProperty()
 
     def build(self):
         sm = SM()
--- server.py
+++ server.py
@@ -13,6 +13,9 @@ class QOTD(LineReceiver):
         self.factory = factory
         self.name = None
         self.ready = False
+        self.playing = False
+        self.score = 0
+        self.wins = 0
 
     def connectionMade(self):
         self.factory.cons.append(self)
@@ -21,8 +24,9 @@ class QOTD(LineReceiver):
         self.factory.names.remove(self.name)
         self.factory.cons.remove(self)
         self.update_players()
+        self.factory.check_playing()
 
-    def dataReceived(self, data):
+    def lineReceived(self, data):
         msg = pickle.loads(data)
         x = msg[0]
         print(x)
@@ -38,14 +42,17 @@ class QOTD(LineReceiver):
                 self.ready = False
 
         if x == 'begin':
-            if not self.factory.starting:
-                self.factory.starting = True
+            if self.factory.finished:
+                self.factory.finished = False
                 self.factory.begin_game()
 
         if x == 'match':
             if not self.factory.won:
                 self.factory.won = True
                 self.factory.next_card(self)
+
+        if x == 'left':
+            self.playing = False
 
     def update_players(self):
         # self.factory.update_player_list()
@@ -62,9 +69,8 @@ class QOTDFactory(Factory):
     def __init__(self):
         self.names = []
         self.cons = []
-        self.starting = False
         self.cards = []
-        self.finished = False
+        self.finished = True
         self.won = False
 
     def buildProtocol(self, addr):
@@ -81,27 +87,59 @@ class QOTDFactory(Factory):
     def begin_game(self):
         if not all(x.ready for x in self.cons):
             print('not ready!')
-            self.starting = False
+            self.finished = True
         else:
-
-            self.cards = dobble.create_cards()
+            self.cards = None
+            self.cards = dobble.create_cards()[:2]
             deckcard = self.cards.pop(0)
             for x in self.cons:
                 card = self.cards.pop(0)
                 x.sendLine(pickle.dumps(['begin', deckcard, card]))
                 print(len(self.cards))
-            self.starting = False
+
             for x in self.cons:
+                x.playing = True
                 x.ready = False
 
     def next_card(self, winner):
         if len(self.cards) > 0:
             deckcard = self.cards.pop(0)
-            for x in self.cons:
+            for x in list(filter(lambda y: y.playing, self.cons)):
                 if x != winner:
-                    x.sendLine(pickle.dumps(['next', deckcard]))
+                    x.sendLine(pickle.dumps(['next', deckcard, winner.name]))
             winner.sendLine(pickle.dumps(['win', deckcard]))
+            winner.score += 1
             self.won = False
+
+        else:
+            winner.score += 1
+            players = list(filter(lambda y: y.playing, self.cons))
+            scores = {y.name: y.score for y in players}
+            result_string = self.find_winner(players)
+            for x in players:
+                x.sendLine(pickle.dumps(['over', scores, result_string]))
+            self.won = False
+
+    def find_winner(self, players):
+        if len(players) > 0:
+            scores = {x: x.score for x in players}
+            max_value = max(scores.values())
+            winner = [k for k, v in scores.items() if v == max_value]
+            if len(winner) == 1:
+                winner[0].wins += 1
+                return f'{winner[0].name} wins!'
+            else:
+                for x in winner:
+                    x.wins += 0.5
+                return f'{[x.name for x in winner]} drew'
+        else:
+            pass
+
+    def check_playing(self):
+        if not any(x.playing for x in self.cons):
+            self.finished = True
+
+
 
 
 endpoint = TCP4ServerEndpoint(reactor, 8001)
